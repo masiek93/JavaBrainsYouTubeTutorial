5c3f32877f35bd340b93a391c60dc7c821d5a989
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-07 14:11:43 +0200

    Triangle class improved

--- src/com/topyfi/javabrains/Triangle.java
+++ src/com/topyfi/javabrains/Triangle.java
@@ -1,7 +1,20 @@
 package com.topyfi.javabrains;
 
 public class Triangle {
+	
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
 	public void draw(){
-		System.out.println("Triangle drawn");
+		System.out.println(getType() + " Triangle drawn");
 	}
 }
