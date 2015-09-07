4caf09269cbfbf3df005225ec8555e3a63f058fa
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-07 14:35:01 +0200

    after 6th lesson, constructor injecton

--- src/com/topyfi/javabrains/Triangle.java
+++ src/com/topyfi/javabrains/Triangle.java
@@ -3,7 +3,26 @@ package com.topyfi.javabrains;
 public class Triangle {
 	
 	private String type;
+	private int height;
+
+	public Triangle(String type){
+		this.type = type;
+	}
 	
+	public Triangle(int height){
+		this.height = height;
+	}
+	
+	public Triangle(String type, int height){
+		this.type = type;
+		this.height = height;
+	}
+	
+	
+	public int getHeight() {
+		return height;
+	}
+
 	public String getType() {
 		return type;
 	}
@@ -15,6 +34,6 @@ public class Triangle {
 
 
 	public void draw(){
-		System.out.println(getType() + " Triangle drawn");
+		System.out.println(getType() + " Triangle drawn of height " + getHeight());
 	}
 }
