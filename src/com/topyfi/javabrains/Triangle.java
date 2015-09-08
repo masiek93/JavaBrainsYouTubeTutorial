c40c10d44917d6b3c56b5c30e293511609270d54
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-08 10:36:33 +0200

    ApplicationContextAware and BeanNameAware implemented to triangle

--- src/com/topyfi/javabrains/Triangle.java
+++ src/com/topyfi/javabrains/Triangle.java
@@ -2,22 +2,59 @@ package com.topyfi.javabrains;
 
 import java.util.List;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware {
+
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	private ApplicationContext context = null;
 	
-	private List<Point> points;
-	
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
-	
-	public void draw(){
-		for(Point point : points){
-			System.out.println("Point = " + point.getX() + " , " + point.getY());
-		}
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	public void draw() {
+		System.out.println("PointA = " + pointA.getX() + " , " + pointA.getY());
+		System.out.println("PointB = " + pointB.getX() + " , " + pointB.getY());
+		System.out.println("PointC = " + pointC.getX() + " , " + pointC.getY());
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context)
+			throws BeansException {
+		this.context = context;
+		
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+		System.out.println("Bean name -> " + beanName);
+		
 	}
 }
