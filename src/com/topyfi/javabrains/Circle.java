613c153a39d5db753a9a65e3d29209b43347098e
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-08 15:05:58 +0200

    autowire annotation added (qualifier) and xml file changed

--- src/com/topyfi/javabrains/Circle.java
+++ src/com/topyfi/javabrains/Circle.java
@@ -1,5 +1,7 @@
 package com.topyfi.javabrains;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
 
 public class Circle implements Shape {
@@ -10,7 +12,9 @@ public class Circle implements Shape {
 		return center;
 	}
 	
-	@Required
+	/*@Required*/
+	@Autowired
+	@Qualifier("circleRelated")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
